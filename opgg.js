f7b2a9f88e761a5315faabed535003e0d585c482
Alvarojcb <alvaro.00000844540@unicap.br>
2023-10-30 20:04:48 -0300

    partealvaro

---
+++ opgg.js
@@ -0,0 +1,28 @@
+var API_key="RGAPI-d22f451d-1ef7-4568-a2ff-ed554ba94925";
+var player_n ="";
+var br1 = "https://br1.api.riotgames.com";
+
+//API da riot games, Ela basicamente mostra a foto de perfil e o nível do jogador que o nick for inserido. Alguns Exemplos para testar a API são: Alvola, GRS Rocha, ElderRuisu, Tatucabeludo.
+function procurar_jogador(){
+    player_n = document.getElementById("player_n").value;
+    console.log(player_n);
+    data()
+}
+
+async function data(){
+    var playerUrl = "/lol/summoner/v4/summoners/by-name/"+player_n;
+    var playerUrlcompleto = br1 + playerUrl+"?api_key="+API_key;
+    console.log(playerUrlcompleto);
+    const dataPlayer_1 = await fetch(playerUrlcompleto);
+    const dataPlayer_Completo = await dataPlayer_1.json();
+    console.log(dataPlayer_Completo);
+
+    var level = dataPlayer_Completo.summonerLevel;
+    console.log(level);
+    document.getElementById("playerlevel_data").innerHTML = player_n +" é level " + level;
+
+    var idperfil = dataPlayer_Completo.profileIconId;
+    var urlperfil = "https://ddragon.leagueoflegends.com/cdn/13.21.1/img/profileicon/"+ idperfil +".png";
+    document.getElementById("summonerprofilepic_picture").src = urlperfil;
+}
+
